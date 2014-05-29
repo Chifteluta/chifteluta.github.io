b246111446bd7904ff1f283e9ccdf1da93cbac0a
Victor <victor@stegaru.com>
2014-05-29 15:24:39 +0300

    Parallax for headings

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ $(document).foundation();
 $('.gallery a').colorbox({
     rel: 'galleryname'
 });
+
+$(window).on('scroll', function() {
+    var y = $(window).scrollTop();
+    $(".is-post .hero").css('background-position', '0px ' + (-50 - y / 3) + 'px');
+});
