6c2a6482b342e0102e6778cc9193e006150b8591
Victor <victor@stegaru.com>
2014-05-29 15:45:40 +0300

    Parallax for headings

--- js/app.js
+++ js/app.js
@@ -4,8 +4,19 @@ $(document).foundation();
 $('.gallery a').colorbox({
     rel: 'galleryname'
 });
-
+var pageHeight = $(window).height();
+var y = $(window).scrollTop() * 100 / pageHeight;
+//$(".is-post .hero").css('background-position', '0px ' + (-50 - y / 3) + 'px');
+$('.is-post .hero').stop().animate({
+    'background-position-x': '0',
+    'background-position-y': 50 + y + "%"
+}, 10000, 'linear');
 $(window).on('scroll', function() {
-    var y = $(window).scrollTop();
-    $(".is-post .hero").css('background-position', '0px ' + (-50 - y / 3) + 'px');
+    var pageHeight = $(window).height();
+    var y = $(window).scrollTop() * 100 / pageHeight;
+    //$(".is-post .hero").css('background-position', '0px ' + (-50 - y / 3) + 'px');
+    $('.is-post .hero').stop().animate({
+        'background-position-x': '0',
+        'background-position-y': 50 + y + "%"
+    }, 500, 'linear');
 });
